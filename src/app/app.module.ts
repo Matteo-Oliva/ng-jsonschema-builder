adaac66e91e80d7c944e2a2fff4198b090edf9f7
PRISMA\matteo.oliva <olivamatteus@gmail.com>
2022-01-20 15:05:46 +0100

    initial fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgJsonSchemaBuilder } from 'ng-json-schema-builder';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { NgJsonSchemaBuilder } from 'ng-json-schema-builder';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    NgJsonSchemaBuilder,
+NgJsonSchemaBuilder,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
